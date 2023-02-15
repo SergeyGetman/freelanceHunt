3cf0ddd43075b961ee5cf593cc8d9f82d1692504
Sergii Getman <sergeygetman2020@gmail.com>
2023-02-15 23:31:21 +0200

    finish first block

--- pages/listmanagers/[id].js
+++ pages/listmanagers/[id].js
@@ -1,17 +1,13 @@
 import {useRouter} from "next/router";
 import MainContainer from "../../components/MainContainer";
-import Link from "next/link";
 import styles from "../../components/styleComponent.module.scss";
 
 
 
  function HeaderImage () {
 
-     const data = [1,2,3,4,5]
-
     const router = useRouter()
     const {query} = router
-     console.log("this is queryID", query)
 
     return (
         <MainContainer>
@@ -30,6 +26,10 @@ import styles from "../../components/styleComponent.module.scss";
                         <div className={styles.general__block__headerIMG_textLogan}>
                             What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.
                         </div>
+                    </div>
+
+                    <div className={styles.general__block_BTN__SignUp}>
+                        <button>SIGN IN</button>
                     </div>
 
                 </div>
