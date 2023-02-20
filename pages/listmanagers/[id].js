a8f8d4a51545fe20c1874a934f7312608ba997f9
Sergii Getman <sergeygetman2020@gmail.com>
2023-02-21 00:14:52 +0200

    add second block

--- pages/listmanagers/[id].js
+++ pages/listmanagers/[id].js
@@ -6,17 +6,16 @@ import React, {useEffect, useState} from "react";
 import {objAPI} from "../../api/api";
 
 
+function HeaderImage() {
 
- function HeaderImage () {
+    const [usersForCircle, setUsersForCircle] = useState([])
+    console.log("this is usersForCircle", usersForCircle)
 
-     const [usersForCircle, setUsersForCircle] = useState([])
-     console.log("this is usersForCircle", usersForCircle)
-
-     useEffect(() => {
-         objAPI.getCircle().then(res => {
-             setUsersForCircle(res.users)
-         })
-     },[])
+    useEffect(() => {
+        objAPI.getCircle().then(res => {
+            setUsersForCircle(res.users)
+        })
+    }, [])
 
     const router = useRouter()
     const {query} = router
@@ -24,46 +23,62 @@ import {objAPI} from "../../api/api";
     return (
         <MainContainer>
             <div className={styles.general__block}>
-             <h1>THIS IS MY OUTS </h1>
-                <div className={styles.general__block_BTN}>
-                    <button>USERS</button>
-                    <button>SIGN IN</button>
-                </div>
+                <h1>THIS IS MY OUTS </h1>
+                <div className={styles.general__block__firstBlock}>
 
-                <div className={styles.general__block__headerIMG} >
-                    <div className={styles.general__block__headerIMG__AllText}>
-                        <div className={styles.general__block__headerIMG_textHEADER}>
-                            Test assignment for front-end developer
-                        </div>
-                        <div className={styles.general__block__headerIMG_textLogan}>
-                            What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.
-                        </div>
-                    </div>
 
-                    <div className={styles.general__block_BTN__SignUp}>
+                    <div className={styles.general__block_BTN}>
+                        <button>USERS</button>
                         <button>SIGN IN</button>
                     </div>
 
-                </div>
-
-                <div className={styles.general__block__users}>
-                    {usersForCircle?.map((el, idx) => {
-                        return (
-                            <div key={idx}>
-                                <UserCircle
-                                image={el.photo}
-                                name={el.name}
-                                position={el.position}
-                                email={el.email}
-                                number={el?.phone}
-                            />
+                    <div className={styles.general__block__headerIMG}>
+                        <div className={styles.general__block__headerIMG__AllText}>
+                            <div className={styles.general__block__headerIMG_textHEADER}>
+                                Test assignment for front-end developer
                             </div>
+                            <div className={styles.general__block__headerIMG_textLogan}>
+                                What defines a good front-end developer is one that has skilled knowledge of HTML, CSS,
+                                JS with a vast understanding of User design thinking as they'll be building web
+                                interfaces with accessibility in mind. They should also be excited to learn, as the
+                                world of Front-End Development keeps evolving.
+                            </div>
+                        </div>
 
-                        )
-                    })}
+                        <div className={styles.general__block_BTN__SignUp}>
+                            <button>SIGN IN</button>
+                        </div>
+
+                    </div>
                 </div>
 
+                <div className={styles.general__block__titleGetRequest}>
+                    Working with GET request
+                </div>
 
+                <div className={styles.general__block__secondBlock}>
+                    <div className={styles.general__block__users}>
+                        {usersForCircle?.map((el, idx) => {
+                            return (
+                                <div className={styles.general__block__users__user} key={idx}>
+                                    <UserCircle
+                                        image={el.photo}
+                                        name={el.name}
+                                        position={el.position}
+                                        email={el.email}
+                                        number={el?.phone}
+                                    />
+                                </div>
+
+                            )
+                        })}
+
+                    </div>
+                    <div className={styles.general__block__users__btn}>
+                        <button>show more</button>
+                    </div>
+
+                </div>
 
 
             </div>
